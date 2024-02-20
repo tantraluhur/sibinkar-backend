e91c3d80b9a150d3550f444c10ed83a980fcfdd6
aprilian tantra <tantraluhur@gmail.com>
2024-02-20 13:09:46 +0700

    init project

---
+++ README.md
@@ -0,0 +1,22 @@
+## Django Project initializer for Ready Work 
+
+## Installation
+
+Follow these steps:
+- Install latest Python3
+- Install `virtualenv` with `pip install virtualenv`
+- Create an env in this folder called `env` (naming is important) `virtualenv ./env`
+- Activate the python environment with `source ./env/bin/activate`
+- Install the python Dependencies with `pip install -r requirements.txt`
+- You are now ready to go!
+
+## Running
+- Copy `env.example` as `.env` and put relevant value
+- `python manage.py runserver`
+- [Postman Collection](https://www.getpostman.com/collections/68de849dd7c6e44e6692)
+
+## Features 
+- Django Boilerplate with django-rest-framework and drf-simple-jwt 
+- Login(email/phone)
+- Registration
+
---
+++ commons/middlewares/exception.py
@@ -0,0 +1,32 @@
+from rest_framework import status
+from typing import Optional, Union
+from pydantic import BaseModel
+
+from rest_framework.exceptions import APIException
+from django.utils.functional import Promise
+
+from applibs.response import prepare_error_response
+
+class APIErrorData(BaseModel) :
+    detail: Union[str, Promise]
+    code: Optional[str] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+
+class ExtendedAPIException(APIException) :
+    def __init__(self, detail=None, code=None):
+        if isinstance(detail, APIErrorData) :
+            super().__init__(detail=(detail.detail), code=detail.code)
+            return
+        super().__init__((detail), code)
+
+
+class BadRequestException(ExtendedAPIException) :
+    status_code = status.HTTP_400_BAD_REQUEST
+
+class NotFoundException(ExtendedAPIException) :
+    status_code = status.HTTP_404_NOT_FOUND
+
+class AuthenticationException(ExtendedAPIException) :
+    status_code = status.HTTP_401_UNAUTHORIZED
