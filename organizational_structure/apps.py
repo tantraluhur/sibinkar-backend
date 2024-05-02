c0ddcaa392f91f09452445f2c34263d948bd4ff9
aprilian tantra <tantraluhur@gmail.com>
2024-05-02 15:08:55 +0700

    feat: init organizational structure app

---
+++ organizational_structure/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OrganizationalStructureConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'organizational_structure'
